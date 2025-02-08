fe74af68767dcaaa61a12a6ca090f628a743619f
Emi Jade <me@emi.lgbt>
2025-02-07 20:21:50 -0800

    Removed themeing and added the packages to home.nix

--- systems/main/home.nix
+++ systems/main/home.nix
@@ -1,18 +1,29 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "emi";
-  home.homeDirectory = "/home/emi";
-
   imports = [
     ./modules/bash.nix
     ./modules/code.nix
-    ./modules/theming.nix
     ./modules/zip.nix
   ];
 
+  home.username = "emi";
+  home.homeDirectory = "/home/emi";
+
   home.packages = with pkgs; [
     winetricks
+
+    yaru-theme
+    gnome-software
+    ubuntu-themes
+    gnome-tweaks
+
+    ubuntu-sans
+    noto-fonts
+    noto-fonts-extra
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
   ];
 
   # basic configuration of git, please change to your own
