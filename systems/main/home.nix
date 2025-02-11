bd3896b2cb9cfab3000e43f4d56cf24ae14708b5
Emi Jade <me@emi.lgbt>
2025-02-11 00:17:33 -0800

    Added nvchad to home-manager for (main)/nixy

--- systems/main/home.nix
+++ systems/main/home.nix
@@ -36,10 +36,10 @@
       dockerfile-language-server-nodejs
       nixd
       fenix
-      (python3.withPackages(ps: with ps: [
+      (python3.withPackages(ps: with ps; [
         python-lsp-server
         flake8
-      ]));
+      ]))
     ];
     hm-activation = true;
     backup = true;
