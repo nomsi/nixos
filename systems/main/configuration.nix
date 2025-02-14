8a19ed02512eff0f45eaa9bd4304b94254b68ca1
Emi Jade <me@emi.lgbt>
2025-02-13 16:08:43 -0800

    Added systemd tmp file rules

--- systems/main/configuration.nix
+++ systems/main/configuration.nix
@@ -160,6 +160,17 @@ virtualisation.libvirtd = {
     };
   };
 
+  systemd.tmpfiles.rules =
+  let
+    firmware =
+      pkgs.runCommandLocal "qemu-firmware" { } ''
+        mkdir $out
+        cp ${pkgs.qemu}/share/qemu/firmware/*.json $out
+        substituteInPlace $out/*.json --replace ${pkgs.qemu} /run/current-system/sw
+      '';
+  in
+  [ "L+ /var/lib/qemu/firmware - - - - ${firmware}" ];
+
   # Garbage Collection
   nix.gc = {
     automatic = true;
